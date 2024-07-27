35e1e26e5c654b369b080c89a4599cd55495308b
jamesm21 <jamesm21@uni.coventry.ac.uk>
2024-07-27 23:36:26 +0100

    Handles command line argument for a C source file and reads it.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs;
+use std::io;
+use std::process;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let source_code = read_source(&args).unwrap_or_else(|_|{
+        println!("Invalid source code file provided.");
+        process::exit(1);
+    });
+    println!("{}", source_code);
+
+}
+
+fn read_source(args: &[String]) -> Result<String, io::Error> {
+    let source_file = args[1].clone();
+    let source_code = fs::read_to_string(source_file)?;
+    Ok(source_code)
 }
---
+++ test_source/addition.c
@@ -0,0 +1,6 @@
+int main()
+{
+    int num1 = 5;
+    int num2 = 4;
+    int num3 = num1 + num2;
+}
